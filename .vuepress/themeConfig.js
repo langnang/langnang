386beefdf329719d04616b4d8c81dd7eebd6ef60
langnang <langnang.chen@outlook.com>
2023-02-09 17:32:44 +0800

    config

---
+++ .vuepress/themeConfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // 导航栏链接
+  nav: [
+    { text: "Home", link: "/" },
+    { text: "Guide", link: "/guide/" },
+    { text: "VuePress", link: "https://vuepress.vuejs.org/" },
+    { text: "GitHub", link: "https://github.com/langnang/langnang/" },
+  ],
+  // 侧边栏
+  sidebar: [],
+  // 显示所有页面的标题链接
+  displayAllHeaders: true,
+  // 最后更新时间
+  lastUpdated: "Last Updated",
+};
