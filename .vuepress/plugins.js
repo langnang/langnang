e819cb95b6c8e81126f1d9763d84fe6a44dd425b
langnang <langnang.chen@outlook.com>
2023-02-10 16:00:35 +0800

    plugin comment

--- .vuepress/plugins.js
+++ .vuepress/plugins.js
@@ -1,4 +1,6 @@
 module.exports = [
+  // provides a global component wrapping mermaid.js
   "vuepress-plugin-mermaidjs",
+  // A VuePress plugin that copies code blocks with one click.
   "@xiaopanda/vuepress-plugin-code-copy",
 ];
