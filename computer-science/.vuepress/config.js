10bf542ee91e80e34664f5994a38b64009dd603c
langnang <langnang.chen@outlook.com>
2023-02-11 09:40:10 +0800

    computer-science

--- computer-science/.vuepress/config.js
+++ computer-science/.vuepress/config.js
@@ -11,6 +11,15 @@ const config = require("./../../.vuepress/config");
 
 config.title = "Computer Science";
 
-config.themeConfig.nav.splice(2, 0, ...[]);
+config.themeConfig.nav.splice(
+  2,
+  0,
+  ...[
+    { text: "算法理论", link: "/52010/5201040/" },
+    { text: "数据结构", link: "/52010/5201050/" },
+    { text: "数据库", link: "/52040/5204050/" },
+    { text: "开发技术", link: "/52040/5204060/" },
+  ]
+);
 
 module.exports = config;
