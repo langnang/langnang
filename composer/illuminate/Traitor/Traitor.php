0028375791a1a0e836c790e05f709735223a99f1
langnang <langnang.chen@outlook.com>
2024-09-02 13:50:44 +0800

    2024-09-02 13:50:42

--- composer/illuminate/Traitor/Traitor.php
+++ composer/illuminate/Traitor/Traitor.php
@@ -13,4 +13,39 @@ namespace Illuminate\Traitor;
  * @name
  * @description
  */
-class Traitor {}
+class Traitor
+{
+  /**
+   * @param  string  $trait
+   * @return TraitUseAdder
+   */
+  public static function addTrait($trait)
+  {
+    $instance = new TraitUseAdder();
+
+    return $instance->addTraits([$trait]);
+  }
+
+  /**
+   * @param  array  $traits
+   * @return TraitUseAdder
+   */
+  public static function addTraits($traits)
+  {
+    $instance = new TraitUseAdder();
+
+    return $instance->addTraits($traits);
+  }
+
+  /**
+   * Check if provided class uses a specific trait.
+   *
+   * @param  string  $className
+   * @param  string  $traitName
+   * @return bool
+   */
+  public static function alreadyUses($className, $traitName)
+  {
+    return in_array($traitName, class_uses($className));
+  }
+}
