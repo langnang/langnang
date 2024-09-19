a9931bdb49c8dc2269371ce6327146417a913884
langnang <langnang.chen@outlook.com>
2024-09-19 10:16:27 +0800

    2024-09-19 10:16:25

--- composer/core/Traits/AliasesTrait.php
+++ composer/core/Traits/AliasesTrait.php
@@ -4,14 +4,42 @@ namespace Core\Traits;
 
 trait AliasesTrait
 {
+  /**
+   * set
+   */
+  function set_aliases($name, $value = null)
+  {
+    if (!is_array($this->aliases)) $this->aliases = [];
+    $this->aliases[$name] = $value;
+  }
+  /**
+   * get
+   */
   function get_aliases($name = null)
   {
     if (empty($this->aliases)) return;
     return empty($name) ? $this->aliases : $this->aliases[$name];
   }
-
+  /**
+   * is
+   */
+  /**
+   * isset
+   */
+  /**
+   * unset
+   */
+  /**
+   * has
+   */
+  /**
+   * exists
+   */
   function alias_exists($name)
   {
     return array_key_exists($name, $this->aliases);
   }
+  /**
+   * to
+   */
 }
