7d8701e0888662f018740d75272380ca0423a92c
langnang <langnang.chen@outlook.com>
2024-09-25 13:32:10 +0800

    2024-09-25 13:32:08

--- composer/illuminate/Console/Console.php
+++ composer/illuminate/Console/Console.php
@@ -7,11 +7,11 @@ class Console extends \Core\Illuminate
   public $commands = [];
   function _autoload()
   {
-    foreach (\glob(dirname(__DIR__) . DIRECTORY_SEPARATOR . "**" . DIRECTORY_SEPARATOR . 'Commands' . DIRECTORY_SEPARATOR . '*Command.php') as $file) {
-      $className = "Illuminate" . substr($file, strlen(dirname(__DIR__)), -4);
-      $commad = new $className;
-      $this->addCommand($commad->getName(), $commad);
-    }
+    // foreach (\glob(dirname(__DIR__) . DIRECTORY_SEPARATOR . "**" . DIRECTORY_SEPARATOR . 'Commands' . DIRECTORY_SEPARATOR . '*Command.php') as $file) {
+    //   $className = "Illuminate" . substr($file, strlen(dirname(__DIR__)), -4);
+    //   $commad = new $className;
+    //   $this->addCommand($commad->getName(), $commad);
+    // }
     // var_dump($this->commands);
   }
 
