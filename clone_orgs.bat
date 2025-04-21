c28a0f6cfb0f3df3d2f0b52e671482778fe7722e
langnang <langnang.chen@outlook.com>
2025-04-21 14:16:14 +0800

    2025-04-21 14:16:11

---
+++ clone_orgs.bat
@@ -0,0 +1,29 @@
+@echo off
+
+git clone git@github.com:langnang-applications/.github.git applications
+
+git clone git@github.com:langnang-assets/.github.git assets
+
+git clone git@github.com:langnang-configs/.github.git configs
+
+git clone git@github.com:langnang-documents/.github.git documents
+
+git clone git@github.com:langnang-examples/.github.git examples
+
+git clone git@github.com:langnang-extensions/.github.git extensions 
+
+git clone git@github.com:langnang-frameworks/.github.git frameworks 
+
+git clone git@github.com:langnang-modules/.github.git modules 
+
+git clone git@github.com:langnang-packages/.github.git packages 
+
+git clone git@github.com:langnang-projects/.github.git projects 
+
+git clone git@github.com:langnang-relax/.github.git relax 
+
+git clone git@github.com:langnang-scripts/.github.git scripts 
+
+git clone git@github.com:langnang-softwares/.github.git softwares 
+
+git clone git@github.com:langnang-templates/.github.git templates 
