04611be058e5ba9c9a3de1feda1327aeb459d600
langnang <langnang.chen@outlook.com>
2025-04-21 14:35:34 +0800

    2025-04-21 14:35:31

--- clone_orgs.bat
+++ clone_orgs.bat
@@ -1,5 +1,8 @@
 @echo off
 
+:: Jump to the directory where this file reside.
+cd /d "%~dp0"
+
 git clone git@github.com:langnang-applications/.github.git applications
 
 git clone git@github.com:langnang-assets/.github.git assets
